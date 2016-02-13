0dbff94ceaa5101cbdd418d8b95ea892f1d6f048
Monika <monika.stobieniecka1@wp.pl>
2016-02-13 17:50:35 +0100

    Pierwsza wersja organizera (toolbar, tabview)

---
+++ app/src/main/java/com/example/android/myapplication/SampleFragmentPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.android.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "To Do", "Calendar", "Notes" };
+
+    public SampleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position)
+    {
+        switch (position)
+        {
+            case 0:
+                return new ToDoFragment();
+            case 1:
+                return new CalendarFragment();
+            case 2:
+                return new NotesFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
